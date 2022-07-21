2b19f1bbdac56422cccf4063d0d73e6ee03aa43d
Jason <jason2580134@outlook.com>
2022-07-20 23:19:57 -0700

    rewritten connection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import axios from 'axios';
+
+const socket = io.connect('localhost:8080');
+
+// get user's public IP -> emit to server to check other online user's with same IP
+
 
 const App = () => {
+
+  const [nearby, setNearby] = useState();
+
+  useEffect(() => {
+    const connectUser = async () => {
+      const res = await axios.get('https://api.ipify.org/?format=json');
+      const IP = await res.data.ip;
+      const OS = navigator.userAgentData.platform;
+    
+      await socket.emit('connect-ip', {
+        ip: IP,
+        os: OS,
+      });
+    }
+    connectUser();
+  }, [])
+
+  socket.on('nearby-users', (nearbyUsers) => {
+    setNearby(nearbyUsers);
+  })
+
+  console.log(nearby);
+
   return (
     <div className="App">
     </div>
